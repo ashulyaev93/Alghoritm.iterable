899f4194e45bd130a45347406e0582f213c01e03
geekbrains <ashulyaev93@mail.ru>
2020-12-08 20:47:00 +0400

    Домашнее задание №4. Тестовый вариант.

---
+++ src/KeysIterable.java
@@ -0,0 +1,36 @@
+import java.util.Iterator;
+
+public class KeysIterable<A> implements Iterable<A> {
+    A[] elements;
+    int nElements;
+
+    private class KeysIterator<A> implements Iterator<A> {
+        int nextElement = 0;
+
+        public boolean hasNext() {
+            return nextElement < nElements;
+        }
+
+        public A next() {
+            A result = (A) elements[nextElement];
+            nextElement = nextElement + 1;
+            return result;
+        }
+
+        public void remove() {
+            if (nextElement < nElements - 1) {
+                System.arraycopy(elements, nextElement + 1,
+                        elements, nextElement, nElements - nextElement - 1);
+            }
+            nElements--;
+        }
+    }
+
+    public Iterator<A> iterator() {
+        return new KeysIterator<A>();
+    }
+
+    public KeysIterable() {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
